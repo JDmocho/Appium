c1a70a00d7d10f2631548bd7f75788b3ac48c2e4
JDmocho <joanna.dmocho@gmail.com>
2020-06-25 22:54:09 +0200

    refactor: aggregate

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import unittest
 from tests.AccountLogin import AccountLogin
+from tests.RegistrationForm import RegistrationForm
+
 
 # Pobierz testy, które chcesz uruchomić
 test_1 = unittest.TestLoader().loadTestsFromTestCase(AccountLogin)
-
+test_2 = unittest.TestLoader().loadTestsFromTestCase(RegistrationForm)
 
 # Stwórz Test Suita łączac testy
 test_suite = unittest.TestSuite([test_1])
+test_suite = unittest.TestSuite([test_2])
 
 # odpal
 unittest.TextTestRunner(verbosity=2).run(test_suite)
