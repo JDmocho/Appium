bf75c152270bc8032931238a2b3cda5cacc1dd30
JDmocho <joanna.dmocho@gmail.com>
2020-07-01 09:50:29 +0200

    refactor: testsuite

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 import unittest
 from tests.AccountLogin import AccountLogin
 from tests.RegistrationForm import RegistrationForm
+from tests.ShopDouglas import ShopDouglas
+
 
 
 # Pobierz testy, które chcesz uruchomić
-test_1 = unittest.TestLoader().loadTestsFromTestCase(AccountLogin)
-test_2 = unittest.TestLoader().loadTestsFromTestCase(RegistrationForm)
+#test_1 = unittest.TestLoader().loadTestsFromTestCase(AccountLogin)
+#test_2 = unittest.TestLoader().loadTestsFromTestCase(RegistrationForm)
+test_3 = unittest.TestLoader().loadTestsFromTestCase(ShopDouglas)
 
 # Stwórz Test Suita łączac testy
-test_suite = unittest.TestSuite([test_1])
-test_suite = unittest.TestSuite([test_2])
+test_suite = unittest.TestSuite([test_3])
+
 
 # odpal
 unittest.TextTestRunner(verbosity=2).run(test_suite)
