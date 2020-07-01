6ce7c3e3cd08b9a019631f615a536a394582c4b5
JDmocho <joanna.dmocho@gmail.com>
2020-07-01 09:28:57 +0200

    chore: modify SetUp

--- tests/AccountLogin.py
+++ tests/AccountLogin.py
@@ -1,7 +1,12 @@
+
+"""
+Test login functionality
+Version 1.0
+Author Joanna Dmochowska
+"""
+
 import os
 import unittest
-
-
 from appium import webdriver
 from time import sleep
 from library import GetData
@@ -11,6 +16,7 @@ from ddt import ddt, data, unpack
 # Get actual DIR
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 apk_file_path = os.path.join(CUR_DIR, '..', 'DouglasParfumKosmetik_v7.8.4_apkpure.com.apk')
+
 
 
 @ddt
@@ -24,10 +30,21 @@ class AccountLogin(unittest.TestCase):
         desired_caps['udid'] = '192.168.1.12:5555'
         desired_caps['apkPackage'] = 'com.douglas.main'
         desired_caps['apkActivity'] = 'com.douglas.startpage.ui.tabbar.TabbarStartpageActivity'
-        desired_caps['noReset'] = True
+        desired_caps['noReset'] = False
+
+        # correct email and password
+        self.email = ''
+        self.password = ''
 
         # connect to Appium
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+        self.driver.is_app_installed('com.douglas.main')
+        sleep(5)
+        self.driver.find_element_by_android_uiautomator(
+            'new UiSelector().textContains("ODMÓW")').click()
+
+
 
     def tearDown(self):
         self.driver.quit()
@@ -38,8 +55,6 @@ class AccountLogin(unittest.TestCase):
     def test_login_empty_data(self):
         '''Login with empty data should fail'''
 
-        self.driver.is_app_installed('com.douglas.main')
-        sleep(5)
         my_douglas_btn = self.driver.find_element_by_id('com.douglas.main:id/actionMyDouglas')
         my_douglas_btn.click()
         sleep(5)
@@ -74,8 +89,6 @@ class AccountLogin(unittest.TestCase):
     def test_incorrect_email(self, email, password):
         '''Login with incorrect email should fail'''
 
-        self.driver.is_app_installed('com.douglas.main')
-        sleep(5)
         my_douglas_btn = self.driver.find_element_by_id('com.douglas.main:id/actionMyDouglas')
         my_douglas_btn.click()
         sleep(5)
@@ -106,8 +119,6 @@ class AccountLogin(unittest.TestCase):
     def test_incorrect_password(self, email, password):
         '''Login with incorrect password should fail'''
 
-        self.driver.is_app_installed('com.douglas.main')
-        sleep(5)
         my_douglas_btn = self.driver.find_element_by_id('com.douglas.main:id/actionMyDouglas')
         my_douglas_btn.click()
         sleep(5)
@@ -128,17 +139,9 @@ class AccountLogin(unittest.TestCase):
         self.assertTrue(error_password)
 
 
-    CUR_DIR = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(CUR_DIR, '..', 'data/correct_data_account.csv')
-
-    @data(*GetData.get_data(file_path))
-    @unpack
-
-    def test_correct_data(self, email, password):
+    def test_correct_data(self):
         '''Logging in with the correct data should be successful'''
 
-        self.driver.is_app_installed('com.douglas.main')
-        sleep(5)
         my_douglas_btn = self.driver.find_element_by_id('com.douglas.main:id/actionMyDouglas')
         my_douglas_btn.click()
         sleep(5)
@@ -146,9 +149,9 @@ class AccountLogin(unittest.TestCase):
         login_btn.click()
         sleep(5)
         fill_email = self.driver.find_element_by_id('com.douglas.main:id/usernameEdit')
-        fill_email.send_keys(email)
+        fill_email.send_keys(self.email)
         fill_password = self.driver.find_element_by_id('com.douglas.main:id/passwordEdit')
-        fill_password.send_keys(password)
+        fill_password.send_keys(self.password)
 
         confirm_btn = self.driver.find_element_by_id('com.douglas.main:id/loginButton')
         confirm_btn.click()
@@ -158,7 +161,7 @@ class AccountLogin(unittest.TestCase):
         welcome = self.driver.find_elements_by_android_uiautomator('new UiSelector().textContains("Witaj")')
         self.assertTrue(welcome)
 
-        testerka = self.driver.find_elements_by_android_uiautomator('new UiSelector().textContains("Testerka")')
+        testerka = self.driver.find_elements_by_android_uiautomator('new UiSelector().textContains("Joanna")')
         self.assertTrue(testerka)
 
 
